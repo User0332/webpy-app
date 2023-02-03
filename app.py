fd2b1e0002950f11f481024c87d02c5f5e87dea2
User0332 <carlzfurtado@gmail.com>
2023-02-03 08:22:15 -0500

    add weby_setup function

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from flask import Flask
 
 app = Flask(__name__, template_folder="html")
 
-app.debug = True
-app.secret_key = token_urlsafe(20)
-app.config["SESSION_TYPE"] = "filesystem"
-app.config["SESSION_PERMANENT"] = False
+def webpy_setup(app: Flask):
+	app.debug = True
+	app.secret_key = token_urlsafe(20)
+	app.config["SESSION_TYPE"] = "filesystem"
+	app.config["SESSION_PERMANENT"] = False
 
-Session(app)
+	Session(app)
